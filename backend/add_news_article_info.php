b84a37963208e761a2be3219e957b5e1f544b0f2
vergil kelley <verg_1@yahoo.com>
2024-08-21 21:10:32 -0500

    first commit:

---
+++ backend/add_news_article_info.php
@@ -0,0 +1,35 @@
+<?php 
+require 'db.php';
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    exit("POST request method required");
+} elseif ($_POST['news_article_title'] == "") {
+    header("Location: ../edit_news_article.php");  
+}elseif($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update_news_article'])) {
+    $news_article_img_alt = $_POST["news_article_img_alt"];
+    $news_article_title = $_POST["news_article_title"];
+    $news_article_credits = $_POST["news_article_credits"];
+    $news_article_text = $_POST["news_article_text"];
+    $news_article_read_more = $_POST["news_article_read_more"];
+    $news_article_read_more_link = $_POST["news_article_read_more_link"];
+    $fileName = $_FILES["image"]["name"];
+
+    $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+    $allowedTypes = array("jpeg", "jpg", "png", "gif");
+    $tempName = $_FILES["image"]["tmp_name"];
+    $targetPath = "../uploads/".$fileName;
+    if(in_array($ext, $allowedTypes)) {
+        if (move_uploaded_file($tempName, $targetPath)) {
+            $sql = "INSERT INTO news_article (news_article_img, news_article_img_alt, news_article_title, news_article_credits, news_article_text, news_article_read_more, news_article_read_more_link) VALUES ('$fileName', '$news_article_img_alt', '$news_article_title', '$news_article_credits', '$news_article_text', '$news_article_read_more', '$news_article_read_more_link')";
+            if(mysqli_query($conn, $sql)){
+                // echo "Your image has been uploaded";
+                header("Location: ../edit_news_article.php");
+            } else {
+                echo "Image was not uploaded";
+            }
+        }else {
+            echo "File is not uploaded";
+        }
+    } else {
+        echo "File type not allowed";
+    }
+} 
